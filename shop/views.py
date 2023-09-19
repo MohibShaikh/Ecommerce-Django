0a3e6ef185343f79c8c6ab20a4035db98cb08415
sufyansidd19 <112391971+sufyansidd19@users.noreply.github.com>
2023-09-19 21:43:44 +0500

    update

--- shop/views.py
+++ shop/views.py
@@ -1,30 +1,27 @@
 from django.shortcuts import render
 from .models import Product, Contact, Orders
 from math import ceil
+# import the logging library
+import logging
 
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
 # Create your views here.
 from django.http import HttpResponse
 
 def index(request):
-    # products = Product.objects.all()
-    # print(products)
-    # n = len(products)
-    # nSlides = n//4 + ceil((n/4)-(n//4))
-
-    allProds = []
-    catprods = Product.objects.values('category', 'id')
-    cats = {item['category'] for item in catprods}
+    products= Product.objects.all()
+    allProds=[]
+    catprods= Product.objects.values('category', 'id')
+    cats= {item["category"] for item in catprods}
     for cat in cats:
-        prod = Product.objects.filter(category=cat)
+        prod=Product.objects.filter(category=cat)
         n = len(prod)
         nSlides = n // 4 + ceil((n / 4) - (n // 4))
         allProds.append([prod, range(1, nSlides), nSlides])
 
-    # params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
-    # allProds = [[products, range(1, nSlides), nSlides],
-    #             [products, range(1, nSlides), nSlides]]
-    params = {'allProds':allProds}
-    return render(request, 'shop/index.html', params)
+    params={'allProds':allProds }
+    return render(request,"shop/index.html", params)
 
 def about(request):
     return render(request, 'shop/about.html')
@@ -34,8 +31,8 @@ def contact(request):
         name = request.POST.get('name', '')
         email = request.POST.get('email', '')
         phone = request.POST.get('phone', '')
-        description = request.POST.get('desc', '')
-        contact = Contact(name=name, email=email, phone=phone, description=description)
+        desc = request.POST.get('desc', '')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc)
         contact.save()
     return render(request, 'shop/contact.html')
 
